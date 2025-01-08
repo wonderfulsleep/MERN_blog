761ff597a55a5e6c2080cefe900dd12db8a77193
Leo J <leojiang2005@gmail.com>
2025-01-07 21:29:54 -0500

    resumed

--- api/models/User.js
+++ api/models/User.js
@@ -1,1 +1,11 @@
 const mongoose = req('mongoose');
+const {Schema, model} = mongoose;
+
+const UserSchema = new Schema({
+    username: {type: String, required: true, min: 4, unique: true},
+    password: {type: String, require: true},
+});
+
+const UserModel = model('User', UserSchema);
+
+module.exports = UserModel;
